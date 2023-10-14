caccb892bc3f6e84a402bc6b026c0c36e4af374b
Usman <usmanali477275@gmail.com>
2023-10-14 17:05:20 +0500

    This is a working Shopping Cart

---
+++ store.js
@@ -0,0 +1,224 @@
+// import array from "./ArrayData"
+let array=[
+    {
+            imgSrc:"https://images.unsplash.com/photo-1581655353564-df123a1eb820?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dCUyMHNoaXJ0c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"White shirt",
+            category:"Shirts",
+            price:"19$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1504117809472-d99bf528ea44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmxhY2slMjBwYW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Black pant",
+            category:"pants",
+            price:"16$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1625585445301-676203ae67fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJsdWUlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"blue shirt",
+            category:"shirts",
+            price:"199$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1525326006910-384ca07920d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGluayUyMHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"pink Shirt",
+            category:"shirts",
+            price:"169$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1593766821405-f605e0f9535f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNyaWNrZXQlMjBiYXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Cricket Bat",
+            category:"Sports",
+            price:"194$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1531415074968-036ba1b575da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y3JpY2tldCUyMGJhdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Cricket Ball",
+            category:"Sports",
+            price:"192$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1562232723-6949c130734d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwY2hhcmdlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Charger",
+            category:"Electronics",
+            price:"195$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1581541234269-03d5d8576c0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Chairs",
+            category:"Furniture",
+            price:"119$"
+    
+    },
+    {
+            imgSrc:"https://plus.unsplash.com/premium_photo-1671269943825-e45b177add8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVkc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Beds",
+            category:"Furniture",
+            price:"189$"
+    
+    },
+    {
+            imgSrc:"https://media.istockphoto.com/id/1445237941/photo/closeup-on-electric-floor-standing-fan-in-the-modern-living-room-in-sunny-hot-summer-day.webp?s=1024x1024&w=is&k=20&c=l7XhnKrqJVIAAAyytnGsfAw9d_k98vuupgRVASmfB4Y=",
+            name:"Fans",
+            category:"Electronics",
+            price:"119$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+            name:"Laptops",
+            category:"Electronics",
+            price:"193$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+            name:"Mobiles",
+            category:"Electronics",
+            price:"819$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1610553556003-9b2ae8ef1b8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmlrZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Bike",
+            category:"Automobiles",
+            price:"219$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Car",
+            category:"Automobiles",
+            price:"419$"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1522338140262-f46f5913618a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1376&q=80",
+            name:"Hair Dryer",
+            category:"Electronics",
+            price:"19$"
+    
+    },
+    ]
+let container = document.querySelector(".container");
+let product=array;
+const createFilteredProducts=(searchValue)=>{
+    let search=searchValue.toLowerCase();//search=""
+   let updatedProducts= product.filter((pro)=>{
+    return(
+        pro.name.toLowerCase().includes(search)||pro.category.toLowerCase().includes(search)
+    )
+   })
+
+//creating HTML for filtered Products
+let cluster=""
+
+updatedProducts.forEach((pro,id)=>{
+    cluster+=`<div class="product">
+    <img src="${pro.imgSrc}" alt="">
+    <div class="itemName">${pro.name}</div>
+    <div class="category">${pro.category}</div>
+    <p class="itemPrice">${pro.price}</p>
+    <button class="addTocartBtn " id=${id}>Add to Cart</button>
+</div>`
+})
+
+  container.innerHTML = cluster;
+
+}
+
+let searchBar = document.getElementById("searchBar");
+searchBar.addEventListener("input", (e) => {
+let value=e.target.value;
+createFilteredProducts(value)
+})
+//initialy all products
+createFilteredProducts("")
+
+let addTocartBtn=container.querySelectorAll(".addTocartBtn")
+for(let i=0;i<addTocartBtn.length;i++){
+   let currentBtn=addTocartBtn[i];
+   currentBtn.addEventListener("click",(e)=>{
+addTocart(e)
+   })
+}
+let cartItems=document.querySelector(".cartItems")
+function addTocart(e){
+   let target=e.target;
+   let id=e.target.id;
+   let price=target.previousElementSibling.textContent.replace("$","")
+   let itemName=target.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+   let imgSrc=target.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.src
+addItemsToCart(price,itemName,imgSrc)
+}
+function addItemsToCart(price,itemName,imgSrc){
+let div=document.createElement("div")
+let title=cartItems.querySelectorAll(".cartTitle")
+for(let i=0;i<title.length;i++){
+        if(title[i].innerText==itemName){
+                alert("Item Already Exist")
+                return
+        }
+}
+ div.className="cartProduct" 
+div.innerHTML=`
+<img src="${imgSrc}" alt="">
+<h2 class="cartTitle">${itemName}</h2>
+<input type="number" value="1">
+<h3 class="cartPrice">${price}$</h3>
+<button class="removeItem">Remove</button>
+`   
+removeBtn=div.querySelector(".removeItem")
+cartItems.appendChild(div)
+updateTotal()
+let quantityValue=cartItems.querySelectorAll("input")
+for(let i=0;i<quantityValue.length;i++){
+
+quantityValue[i].addEventListener("change",(e)=>{
+        let value=e.target.value
+        if(value<=0){
+        quantityValue[i].value=1
+                value=1
+        }
+        updateTotal(value)
+})
+
+}
+removeBtn.addEventListener("click",removeItem)
+
+}
+let totalBill;
+function updateTotal(qvalue=1){
+        let total=0
+        let totalPrice=document.querySelector(".totalPrice p")
+        let cartPrice=cartItems.querySelectorAll(".cartPrice")
+      for(let i=0;i<cartPrice.length;i++){
+ let value=Number(cartPrice[i].innerText.replace("$",""))
+ 
+total+=value*qvalue;
+      }
+totalPrice.innerHTML="$"+total
+
+}
+
+
+function removeItem(e){
+     e.target.parentElement.remove()
+      updateTotal(1)
+}
+let purchaseBtn=document.querySelector(".purchaseBtn")
+purchaseBtn.addEventListener("click",()=>{
+        let products=cartItems.querySelectorAll(".cartProduct")
+      for(let i=0;i<products.length;i++){
+        products[i].remove()
+      }
+      alert("Thanks for your purchase ")
+      updateTotal()
+
+})
